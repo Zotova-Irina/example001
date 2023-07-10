2d4f8c78f96325ab879af882d759a6a6a86a93f8
Zotova-Irina <irina_botyanovskaya_gr@mail.ru>
2023-07-10 13:08:40 +0700

    Add example005 if else

---
+++ Example005_Conditions_if/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string userName = Console.ReadLine();
+
+if (userName.ToLowerInvariant() == "маша")
+{
+    Console.WriteLine("Ура! Это же Маша!");
+}
+    else
+    {
+        Console.WriteLine($"Привет {userName}!");
+    }
